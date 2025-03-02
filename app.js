6972de7c1107dec7428720982cc0154e547044ae
Govinda Timsina <govindatimsina@outlook.com>
2025-03-02 10:35:48 +0545

    cors setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.static("public"));
@@ -11,6 +12,7 @@ const swaggerUrl =
 
 //regular middlewares
 app.use(express.json({ limit: "20kb" }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "20kb" })); //This will make us able to get data from the url.
 
 //entry point
